0c7006a1ca8e6f76f2c674c2bcd163425ec12b76
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-06-20 17:28:26 +0600

    add backend server and home route

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import 'dotenv/config';
+import cors from 'cors';
+const app = express();
+
+// Middlewares
+app.use(cors())
+app.use(express.json())
+
+// Routes
+app.get('/', (req, res) => res.send('Api is working'))
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log('Server is running on port ' + PORT)
+})
+
+export default app;
