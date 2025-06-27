a93f4f4be984726e3ec0eb3b517cd7f4df9802c8
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-06-27 13:43:08 +0600

    add Google Gemini AI for Blog Post

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import imageKit from '../configs/imageKit.js';
 import Blog from '../models/Blog.js'
 import Comment from '../models/Comment.js';
+import main from '../configs/gemini.js';
 
 export const addBlog = async (req, res) => {
     try {
@@ -106,5 +107,15 @@ export const getBlogComments = async (req, res) => {
         res.json({success: true, comments})
     }catch(error){
         res.json({success:false, message:error.message})
+    }
+}
+
+export const generateContent = async (req, res) => {
+    try {
+        const {prompt} = req.body;
+        const content = await main(prompt + ' Generate a blog content for this topic in simple text format')
+        res.json({success: true, content})
+    } catch (error) {
+        res.json({success: false, message: error.message})
     }
 }
